b8ad17f686a013023933d9bc153b5ab42335a402
Victor Baptista <victor.baptista@mzgorup.com>
2019-09-11 11:59:32 -0300

    GitHub API Project

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var input = document.querySelector('input');
+
+input.onkeyup = function (event) {
+    if (event.keyCode === 13) {
+        user = this.value;
+
+        if (user.length > 0) {
+            axios.get('https://api.github.com/users/' + user, {
+                    headers: {
+                        'Authorization': 'token d1681b1ad3448dbcf7c6104c61f3e110dcf86e42'
+                    }
+                })
+                .then(function (response) {
+
+                    var name = response.data.login;
+                    var pic = response.data.avatar_url;
+                    var link = response.data.html_url;
+                    var desc = response.data.bio;
+
+                    createCard(name,pic,link,desc);
+
+                    console.log(response.data)
+                },
+                (error) => {
+                    alert(error);
+                });
+                
+        }
+    }
+}
+
+
+
+createCard = (name,pic,link,desc) => {
+   const card = document.querySelector('.card');
+
+    // var nameHTML = document.createElement('h2');
+    // nameHTML.id = "name";
+    // var node = document.createTextNode(name);
+    // nameHTML.appendChild(node);
+    // card.appendChild(nameHTML);
+
+    const nameHTML = document.querySelector('#name');
+    const picHTML  = document.querySelector('#pic');
+    const linkHTML = document.querySelector('#link');
+    const descHTML = document.querySelector('#desc');
+
+    nameHTML.innerHTML = name;
+    picHTML.setAttribute('src', pic);
+    linkHTML.setAttribute('href', link);
+    descHTML.innerHTML = desc;
+}
